394796f167562dd6f86093957915fb37b70a871c
azhida <wghzhida@126.com>
2019-03-03 04:43:35 +0000

    添加 用户动态

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,18 +1,33 @@
 @extends('layouts.default')
 
 @section('content')
-    <div class="jumbotron">
-        <h1>Hello 坡马代</h1>
-        <p class="lead">
-            你现在所看到的是 满目青山的<a href="">坡马代风景</a> 和 热情好客的<a href="">坡马代人</a>。
-        </p>
-        <p>
-            一切，将从这里开始。
-        </p>
-        @if (!Auth::check())
+    @if (Auth::check())
+        <div class="row">
+            <div class="col-md-8">
+                <section class="status_form">
+                    @include('shared._status_form')
+                </section>
+                <h3>微博列表</h3>
+                @include('shared._feed')
+            </div>
+            <aside class="col-md-4">
+                <section class="user_info">
+                    @include('shared._user_info', ['user' => Auth::user()])
+                </section>
+            </aside>
+        </div>
+    @else
+        <div class="jumbotron">
+            <h1>Hello 坡马代</h1>
+            <p class="lead">
+                你现在所看到的是 满目青山的<a href="">坡马代风景</a> 和 热情好客的<a href="">坡马代人</a>。
+            </p>
+            <p>
+                一切，将从这里开始。
+            </p>
             <p>
                 <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
             </p>
-        @endif
-    </div>
+        </div>
+    @endif
 @stop
